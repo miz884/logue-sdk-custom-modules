0732a040d3c6d5b19d0853a0587e2333190bfdc3
Kosuke Mizubayashi <miz@mizba.net>
2020-01-09 23:32:24 +0900

    Add a modulation with terrain wave.

---
+++ terrain_wave/logue_nts-1_terrain_wave/modfx/project.mk
@@ -0,0 +1,23 @@
+# #############################################################################
+# Project Customization
+# #############################################################################
+
+LOGUE_SDK_ROOT = ../../../../logue-sdk
+
+PLATFORM_TYPE = nutekt-digital
+
+MODULE_TYPE = modfx
+
+PROJECT = terrain_modfx
+
+UCSRC = 
+
+UCXXSRC = terrain_modfx.cpp ../common/wavetables.cpp
+
+UINCDIR =
+
+UDEFS =
+
+ULIB = 
+
+ULIBDIR =
---
+++ terrain_wave/logue_nts-1_terrain_wave/modfx/terrain_modfx.cpp
@@ -0,0 +1,76 @@
+#include "usermodfx.h"
+#include "../common/wavetables.h"
+
+
+#ifndef k_samplerate
+#define k_samplerate        (48000)
+#endif
+
+#ifndef k_samplerate_recipf
+#define k_samplerate_recipf (2.08333333333333e-005f)
+#endif
+
+typedef struct State {
+  float phase;
+  float param_time;
+  float param_depth;
+  uint16_t wavetable_index;
+} State;
+
+static State state;
+
+void MODFX_INIT(uint32_t platform, uint32_t api) {
+  state.phase = 0.f;
+  state.param_time = 0.f;
+  state.param_depth = 0.f;
+  state.wavetable_index = 0;
+
+  init_wavetables();
+}
+
+void MODFX_PROCESS(const float *main_xn, float *main_yn,
+                   const float *sub_xn,  float *sub_yn,
+                   uint32_t frames) {
+  const float *mx = main_xn;
+  float * __restrict my = main_yn;
+  const float * my_e = my + 2 * frames;
+
+  const float *sx = sub_xn;
+  float * __restrict sy = sub_yn;
+
+  float phase = state.phase;
+  const float p_time = state.param_time;
+  const float p_depth = state.param_depth;
+
+  for (; my != my_e; ) {
+    float wave = get_wave_value(state.wavetable_index, phase);
+    wave = (wave + 1.f) / 2.f * p_depth + (1.f - p_depth / 2.f);;
+    
+    *(my++) = wave * *(mx++);
+    *(my++) = wave * *(mx++);
+    *(sy++) = wave * *(sx++);
+    *(sy++) = wave * *(sx++);
+
+    phase += k_samplerate_recipf * (p_time * 9.f + 1.f);
+  }
+
+  state.phase = phase;
+  state.param_time = p_time;
+  state.param_depth = p_depth;
+}
+
+
+void MODFX_PARAM(uint8_t index, int32_t value) {
+  const float valf = q31_to_f32(value);
+  switch (index) {
+  case k_user_modfx_param_time:
+    state.param_time = valf;
+    break;
+  case k_user_modfx_param_depth:
+    state.param_depth = valf;
+    break;
+  default:
+    break;
+  }
+}
+
