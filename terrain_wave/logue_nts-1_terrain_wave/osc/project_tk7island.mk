d53d33b91ec394306bb49353fec284016dabac66
Kosuke Mizubayashi <miz@mizba.net>
2020-01-28 21:35:38 +0900

    Add wavetables based on Tokyo 7 Islands land shapes.
    And an oscillator with the 7 islands wavetables.

---
+++ terrain_wave/logue_nts-1_terrain_wave/osc/project_tk7island.mk
@@ -0,0 +1,25 @@
+# #############################################################################
+# Project Customization
+# #############################################################################
+
+LOGUE_SDK_ROOT = ../../../../logue-sdk
+
+PLATFORM_TYPE = nutekt-digital
+
+MODULE_TYPE = osc
+
+PROJECT = tk7island
+
+UCSRC = 
+
+UCXXSRC = tk7island_wave.cpp ../common/tk7island.cpp
+
+UINCDIR = 
+
+UDEFS = -DSIMPLE_WAVETABLES
+
+ULIB = 
+
+ULIBDIR = 
+
+MANIFEST = manifest_tk7island.json
---
+++ terrain_wave/logue_nts-1_terrain_wave/osc/tk7island_wave.cpp
@@ -0,0 +1,102 @@
+#include "userosc.h"
+#include "../common/tk7island.h"
+#include "../common/wavetables.h"
+
+typedef struct State {
+  float phase;
+  uint8_t flags;
+  uint8_t wavetable_index;
+  uint32_t step_count;
+  uint8_t eg_enabled;
+  float eg_scale;
+} State;
+
+static State state;
+
+enum {
+  flags_clear = 0,
+  flag_noteon = 1 << 0,
+  flag_noteoff = 1 << 1,
+};
+
+void OSC_INIT(uint32_t platform, uint32_t api) {
+  state.phase = 0.f;
+  state.flags = flags_clear;
+  state.wavetable_index = 0;
+  state.step_count = 0;
+  state.eg_enabled = 0;
+  state.eg_scale = 1.0;
+
+  init_wavetables();
+}
+
+void OSC_CYCLE(const user_osc_param_t* params,
+               int32_t* yn,
+               const uint32_t frames) {  
+  // Handle the reset flag on NOTEON.
+  if (state.flags & flag_noteon) {
+    state.flags = flags_clear;
+    state.phase = 0.f;
+    state.step_count = 0;
+  }
+
+  // Restore the last state.
+  float phase = state.phase;
+  uint32_t steps = state.step_count;
+  uint8_t eg_enabled = state.eg_enabled;
+  float eg_scale = state.eg_scale;
+
+  // Calculate the current phase.
+  const float w_delta = osc_w0f_for_note((params->pitch)>>8, params->pitch & 0xFF);
+
+  // Prepare the result buffer.
+  q31_t* __restrict y = (q31_t*) yn;
+  const q31_t* y_e = y + frames;
+
+  for (; y != y_e; ) {
+
+    // Main signal
+    const float drive = (get_wave_value(state.wavetable_index, (float) steps / k_samplerate * eg_scale) + 1.0) / 2.0;
+    const float sig  = osc_softclipf(0.05f, (eg_enabled == 1 ? drive : 1.0) * get_wave_value(state.wavetable_index, phase));
+    *(y++) = f32_to_q31(sig);
+
+    // Next step.
+    phase += w_delta;
+    phase -= (uint32_t) phase; // to keep phase within 0.0-1.0.
+    if (steps < k_samplerate / eg_scale) ++steps;
+  }
+
+  // Store the state.
+  state.phase = phase;
+  state.step_count = steps;
+}
+
+void OSC_NOTEON(const user_osc_param_t * const params) {
+  state.flags |= flag_noteon;
+}
+
+void OSC_NOTEOFF(const user_osc_param_t * const params) {
+  state.flags |= flag_noteoff;
+}
+
+void OSC_PARAM(uint16_t index, uint16_t value) {
+  switch (index) {
+  case k_user_osc_param_id1:
+    state.wavetable_index = value;
+    break;
+  case k_user_osc_param_id2:
+    state.eg_enabled = value;
+    break;
+  case k_user_osc_param_id3:
+    state.eg_scale = (float) value;
+    break;
+  case k_user_osc_param_id4:
+  case k_user_osc_param_id5:
+  case k_user_osc_param_id6:
+  case k_user_osc_param_shape:
+  case k_user_osc_param_shiftshape:
+  default:
+    break;
+  }
+}
+
